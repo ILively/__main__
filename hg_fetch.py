8f15cf71e847c09a47f20da01d4b1feeca893c59
aiyaas <167105730+aiyaas@users.noreply.github.com>
2025-12-30 19:03:08 +0700

    Fetching data from server to JSON format

--- hg_fetch.py
+++ hg_fetch.py
@@ -3,7 +3,7 @@ from json import dumps
 
 # 0.0.1 12-30-2025
 # Launch to process JSON data from server
-def fetch_hg(URL: str, file_name: str) -> object:
+def hg_fetch(URL: str, file_name: str) -> object:
     """
     Fetch and save Hugging Face dataset rows to a local JSON file.
 
@@ -28,5 +28,5 @@ def fetch_hg(URL: str, file_name: str) -> object:
         pass
 
 if __name__ == "__main__":
-    fetch_hg("https://datasets-server.huggingface.co/rows?dataset=nvidia%2FNemotron-Pretraining-Dataset-sample&config=Nemotron-CC-Diverse-QA&split=train&offset=0&length=100", "Nemotron-CC-Diverse-QA.json")
+    hg_fetch("https://datasets-server.huggingface.co/rows?dataset=nvidia%2FNemotron-Pretraining-Dataset-sample&config=Nemotron-CC-Diverse-QA&split=train&offset=0&length=100", "Nemotron-CC-Diverse-QA.json")
 
